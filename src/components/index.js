6f39e771d6823500b79465fd3c9148735f524b6f
amirhossein <79211332+capamir@users.noreply.github.com>
2025-07-05 17:22:44 +0330

    components added

---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+export { default as BarCahrt } from "./BarChart"
+export { default as GeographyChart } from "./GeographyChart"
+export { default as Header } from "./Header"
+export { default as LineChart } from "./LineChart"
+export { default as PieChart } from "./PieChart"
+export { default as ProgressCircle } from "./ProgressCircle"
+export { default as StatBox } from "./StatBox"
+
