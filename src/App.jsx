58fd37455616e463f9aef01f25976dbec47bf5e3
amirhossein <79211332+capamir@users.noreply.github.com>
2025-07-06 11:58:12 +0330

    refactoring invoices page

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { ColorModeContext, useMode } from "./theme";
-import { Dashboard, Sidebar, Team, Contacts } from "./scenes";
+import { Dashboard, Sidebar, Team, Contacts, Invoices } from "./scenes";
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -20,8 +20,8 @@ function App() {
               <Route path="/" element={<Dashboard />} />
               <Route path="/team" element={<Team />} />
               <Route path="/contacts" element={<Contacts />} />
-              {/* <Route path="/invoices" element={<Invoices />} />
-              <Route path="/form" element={<Form />} />
+              <Route path="/invoices" element={<Invoices />} />
+              {/* <Route path="/form" element={<Form />} />
               <Route path="/bar" element={<Bar />} />
               <Route path="/pie" element={<Pie />} />
               <Route path="/line" element={<Line />} />
