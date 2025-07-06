71674dddc8791029c11530bcd1a4ac9a7010a603
amirhossein <79211332+capamir@users.noreply.github.com>
2025-07-06 19:04:27 +0330

    refactoring bar chart

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { ColorModeContext, useMode } from "./theme";
-import { Dashboard, Sidebar, Team, Contacts, Invoices } from "./scenes";
+import { Dashboard, Sidebar, Team, Contacts, Invoices, Form, Bar } from "./scenes";
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -21,9 +21,9 @@ function App() {
               <Route path="/team" element={<Team />} />
               <Route path="/contacts" element={<Contacts />} />
               <Route path="/invoices" element={<Invoices />} />
-              {/* <Route path="/form" element={<Form />} />
+              <Route path="/form" element={<Form />} />
               <Route path="/bar" element={<Bar />} />
-              <Route path="/pie" element={<Pie />} />
+              {/* <Route path="/pie" element={<Pie />} />
               <Route path="/line" element={<Line />} />
               <Route path="/faq" element={<FAQ />} />
               <Route path="/calendar" element={<Calendar />} />
